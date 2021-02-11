ace7ef5c0376fe2b2095a7992d2aa7bc6104b75f
John Chamberlin <johnchamberlin00@gmail.com>
2021-02-11 12:50:16 -0600

    monolith added

--- api/server.js
+++ api/server.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const server = express()
 const cors = require('cors')
+const path = require('path')
 
 server.use(express.json())
 server.use(cors())
+server.use(express.static(path.join(__dirname, 'client/build', 'index.html')))
 
 server.use('/api*', (_, res) => {
     res.json({ data: 'The api is working, gj ig' })
---
+++ client/src/App.js
@@ -0,0 +1,26 @@
+import './App.css'
+import { useState, useEffect } from 'react'
+
+const url = path => {
+  return process.env.NODE_ENV === 'development'
+    ? `http://localhost:4000${path}`
+    : path
+}
+
+function App() {
+  const [data, setData] = useState('sup')
+
+  useEffect(() => {
+    fetch(url('/api'))
+      .then(res => res.json())
+      .then(api => setData(api.data))
+  }, [])
+
+  return (
+    <div className='App'>
+      <header className='App-header'>API Data: {data} </header>
+    </div>
+  )
+}
+
+export default App
